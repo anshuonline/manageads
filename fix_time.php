1d061c6102b5f5c6c896fbae6b5e5a97e7cea7ab
anshuonline <helloanshu.dev@gmail.com>
2026-09-19 18:00:07 +0530

    temp: fix time syntax

--- fix_time.php
+++ fix_time.php
@@ -1,1 +1,5 @@
-﻿<?php require 'config.php'; $conn->query('UPDATE search_analytics_log SET searched_at = DATE_ADD(searched_at, INTERVAL 5 HOUR + 30 MINUTE)'); echo 'Time fixed!'; ?>
+<?php
+require 'config.php';
+$conn->query("UPDATE search_analytics_log SET searched_at = DATE_ADD(searched_at, INTERVAL '5:30' HOUR_MINUTE)");
+echo 'Time fixed!';
+?>
