4fb139521b74fdec5bc124e5e426c852338e5dff
anshuonline <helloanshu.dev@gmail.com>
2026-09-23 22:01:51 +0530

    fix: Ensure fake streams descend logically by rank

--- mailer.php
+++ mailer.php
@@ -236,7 +236,8 @@ class GanaTubeMailer {
             $vid = htmlspecialchars($song['video_id'] ?? '', ENT_QUOTES, 'UTF-8');
             $rawTitle = trim($song['title'] ?? 'Trending Track');
             $title = htmlspecialchars($rawTitle, ENT_QUOTES, 'UTF-8');
-            $playCount = (int)($song['play_count'] ?? 0) + rand(125000, 9850000);
+            $fakeBase = 9000000 - ($idx * 2000000);
+            $playCount = (int)($song['play_count'] ?? 0) + rand($fakeBase, $fakeBase + 999999);
             $rawArtist = trim($song['artist'] ?? '');
             
             if (!empty($rawArtist)) {
